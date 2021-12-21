30925502b95c639f63f495707c7aa12ea946a278
Palaparthy Adityachandra <aditya94palaparthy@gmail.com>
2021-12-21 10:33:55 +0530

    Update settings.py

--- dynamic_form/dynamic_form/settings.py
+++ dynamic_form/dynamic_form/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)+rjfrg739s%n4f+w+)auiakdu0a#=!2f3jd$x5@iq^&h4&=fo'
+SECRET_KEY = '<secret-key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
